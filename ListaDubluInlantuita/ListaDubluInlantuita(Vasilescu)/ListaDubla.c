3c8c268814cdd96dbff7488cd8f3dc0bcb267e76
IulianaBalcescu <114941721+IulianaBalcescu@users.noreply.github.com>
2024-03-31 16:58:05 +0300

    Add files via upload

---
+++ ListaDubluInlantuita/ListaDubluInlantuita(Vasilescu)/ListaDubla.c
@@ -0,0 +1,339 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<malloc.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+
+typedef struct Angajat Angajat;
+typedef struct Nod Nod;
+typedef struct LDI LDI;
+
+struct Angajat
+{
+	char* nume;
+	int varsta;
+};
+
+struct Nod
+{
+	Angajat info;
+	Nod* next;
+	Nod* prev;
+};
+
+struct LDI
+{
+	Nod* prim;
+	Nod* ultim;
+};
+
+void afisareAngajat(Angajat angajat) {
+	printf("\nAngajatul %s are %d ani.", angajat.nume, angajat.varsta);
+}
+
+Angajat initializareAngajat(const char* nume, int varsta) {
+	Angajat a;
+	a.varsta = varsta;
+	a.nume = (char*)malloc(sizeof(char) * strlen(nume) + 1);
+	strcpy(a.nume, nume);
+
+	return a;
+}
+
+void inserareInceput(Angajat angajat, LDI* lista) {
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->info = angajat;
+	nou->next = lista->prim;
+	nou->prev = NULL;
+
+	if (lista->prim) {
+		lista->prim->prev = nou;
+		lista->prim = nou;
+	}
+	else
+	{
+		lista->prim = lista->ultim=nou;
+	}
+}
+
+void inserareFinal(Angajat angajat, LDI* lista) {
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->info = angajat;
+	nou->prev = lista->ultim;
+	nou->next = NULL;
+
+	if (lista->ultim) {
+		lista->ultim->next = nou;
+		lista->ultim = nou;
+	}
+	else
+	{
+		lista->prim = lista->ultim = nou;
+	}
+}
+
+void traversareLista(LDI lista, int* nrAngajati) {
+	while (lista.prim)
+	{
+		afisareAngajat(lista.prim->info);
+		(*nrAngajati)++;
+		lista.prim = lista.prim->next;
+	}
+}
+
+void traversareListaCirculara(LDI lista) {
+	if (!lista.prim)return;
+
+	Nod* it = lista.prim;
+	do {
+
+		afisareAngajat(lista.prim->info);
+		lista.prim = lista.prim->next;
+	} while (lista.prim!=it);
+}
+
+Angajat angajatCuVarstaMinima(LDI lista) {
+	Angajat angajatMin = lista.prim->info;
+	Nod* current = lista.prim->next;
+	while (current)
+	{
+		if (current->info.varsta < angajatMin.varsta) {
+			angajatMin = current->info;
+		}
+		current = current->next;
+	}
+	return angajatMin;
+}
+
+Angajat angajatCuVarstaMaxima(LDI lista) {
+	Angajat  angajatMax = lista.prim->info;
+	Nod* current = lista.prim->next;
+
+	while (current)
+	{
+		if (current->info.varsta > angajatMax.varsta) {
+			angajatMax = current->info;
+		}
+		current = current->next;
+	}
+	return angajatMax;
+}
+
+void conversieListaLaVector(LDI lista, Angajat** vectorAngajat) {
+	int nrAngajati = 0;
+	while (lista.prim)
+	{
+		(*vectorAngajat)[nrAngajati] = initializareAngajat(lista.prim->info.nume, lista.prim->info.varsta);
+		nrAngajati++;
+		lista.prim = lista.prim->next;
+	}
+}
+
+float calculVastaMedie(LDI lista) {
+	if (lista.prim == NULL) {
+		printf("Lista este valida!");
+		return 0.0;
+	}
+
+	int sumaVarsta = 0;
+	int numarAngajati = 0;
+	Nod* current = lista.prim;
+	while (current)
+	{
+		sumaVarsta += current->info.varsta;
+		numarAngajati++;
+		current = current->next;
+	}
+
+	return (float)sumaVarsta / numarAngajati;
+}
+
+void stergereNodDupaNume(LDI* lista, const char* nume) {
+	Nod* current = lista->prim;
+	while (current!=NULL)
+	{
+		if (strcmp(current->info.nume, nume) == 0) {
+			Nod* temp = current->next;
+			if (temp == NULL) {
+				lista->ultim = current->prev;
+			}
+			else
+			{
+				temp->prev = current->next;
+			}if (current->prev==NULL)
+			{
+				lista->prim = temp;
+			}
+			else {
+				current->prev->next = temp;
+			}
+			free(current->info.nume);
+			free(current);
+			printf("\nNodul cu numele %s a fost sters din lista.\n", nume);
+			return;
+		}
+		current = current->next;
+	}
+	
+}
+ 
+void stergereLista(LDI* lista) {
+	while (lista->prim!=NULL)
+	{
+		Nod* temp = lista->prim;
+		lista->prim = lista->prim->next;
+		free(temp->info.nume);
+		free(temp);
+	}
+	lista->ultim = NULL;
+
+}
+int numarTotalDeAngajati(LDI lista) {
+	int numarAngajati = 0;
+	Nod* current = lista.prim;
+	while (current)
+	{
+		numarAngajati++;
+		current = current->next;
+	}
+	return numarAngajati;
+}
+
+void inserareNodLaPozitie(Angajat angajat, int pozitie, LDI* lista) {
+	if (pozitie < 0) {
+		printf("Pozitia trebuie sa fie un numar pozitiv sau zero!\n");
+		return;
+	}
+
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	if (nou == NULL) {
+		printf("Eroare la alocare memorie!\n");
+		exit(EXIT_FAILURE);
+	}
+	nou->info = angajat;
+
+	if (pozitie == 0) { // Inserare la inceputul listei
+		nou->prev = NULL;
+		nou->next = lista->prim;
+		if (lista->prim) {
+			lista->prim->prev = nou;
+		}
+		else {
+			lista->ultim = nou; // Lista era vida, deci acesta este și ultimul element acum
+		}
+		lista->prim = nou;
+	}
+	else {
+		Nod* current = lista->prim;
+		int index = 0;
+		while (current && index < pozitie - 1) { // Ne poziționăm la nodul de dinaintea poziției de inserare
+			current = current->next;
+			index++;
+		}
+		if (current == NULL && pozitie > 0) { // Dacă am ajuns la sfârșitul listei și poziția este mai mare decât numărul de noduri
+			printf("Pozitia depaseste lungimea listei!\n");
+			free(nou);
+			return;
+		}
+		nou->prev = current;
+		nou->next = current->next;
+		if (current->next) { // Dacă inserăm între noduri
+			current->next->prev = nou;
+		}
+		else { // Dacă inserăm la sfârșitul listei
+			lista->ultim = nou;
+		}
+		current->next = nou;
+	}
+	printf("Angajatul %s a fost inserat in lista pe pozitia %d.\n", angajat.nume, pozitie);
+}
+
+void citireAngajati(const char* numeFisier, LDI* lista) {
+	FILE* fisier = fopen(numeFisier, "r");
+	if (fisier == NULL) {
+		printf("Eroare la deschiderea fisierului!\n");
+		return;
+	}
+
+	char linie[100];
+	while (fgets(linie, sizeof(linie), fisier)) {
+		char* nume = strtok(linie, ",");
+		int varsta = atoi(strtok(NULL, ","));
+		inserareFinal(initializareAngajat(nume, varsta), lista);
+	}
+
+	fclose(fisier);
+}
+
+
+void dezalocare(LDI* lista) {
+	Nod* copie = lista->prim;
+	while (copie)
+	{
+		free(copie->info.nume);
+		copie = copie->next;
+		if (copie) {
+			free(copie->prev);
+		}
+	}
+	free(lista->ultim);
+	lista->prim = lista->ultim = NULL;
+}
+
+void main() {
+	LDI lista;
+	lista.prim = NULL;
+	lista.ultim = NULL;
+
+	citireAngajati("angajati.txt", &lista);
+
+	inserareInceput(initializareAngajat("Matei", 28), &lista);
+	inserareInceput(initializareAngajat("Anca", 29), &lista);
+	inserareInceput(initializareAngajat("Maria", 25), &lista);
+	inserareInceput(initializareAngajat("Catalin", 28), &lista);
+
+
+	inserareFinal(initializareAngajat("Andrei", 30), &lista);
+
+	int nrAngajati = 0;
+	traversareLista(lista, &nrAngajati);
+
+	Angajat angajatMin = angajatCuVarstaMinima(lista);
+	printf("\nAngajatul cu varsta cea mai mica este: %s cu varsta %d ani.\n", angajatMin.nume, angajatMin.varsta);
+
+	Angajat angajatMax = angajatCuVarstaMaxima(lista);
+	printf("\Angajatul cu cea mai mare varsta este: %s cu varsta %d ani.\n", angajatMax.nume, angajatMax.varsta);
+
+	printf("\nVarsta medie a angajatilor este : %.2f ani", calculVastaMedie(lista));
+
+
+	Angajat* vectorAngajati = (Angajat*)malloc(sizeof(Angajat) * nrAngajati);
+	conversieListaLaVector(lista, &vectorAngajati);
+	printf("\n Afisare angajati din vectorul de angajati:");
+	for (int i = 0; i < nrAngajati; i++) {
+		afisareAngajat(vectorAngajati[i]);
+		free(vectorAngajati[i].nume);
+	}
+	free(vectorAngajati);
+
+
+
+	stergereNodDupaNume(&lista, "Anca");
+	printf("\nDupa stregere Ancai: ");
+	traversareLista(lista, &nrAngajati);
+
+	inserareNodLaPozitie(initializareAngajat("Ioana", 27), 2, &lista);
+	printf("\nDupa inserarea lui Ioana la pozitia 2:");
+	traversareLista(lista, &nrAngajati);
+
+
+
+	lista.prim->prev = lista.ultim;
+	lista.ultim->next = lista.prim;
+
+	printf("\n\nLista circulara:\n");
+	traversareListaCirculara(lista);
+
+	/*stergereLista(&lista);
+	dezalocare(&lista);*/
+}
---
+++ ListaSimpluInlanuita/ListaSimpluInlanuita(Vasilescu)/ListaSimpla.c
@@ -0,0 +1,271 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<malloc.h>
+#include<string.h>
+#include<stdlib.h>
+
+typedef struct Angajat Angajat;
+typedef struct Nod Nod;
+
+struct Angajat
+{
+	char* nume;
+	int varsta;
+};
+
+struct Nod
+{
+	Angajat info;
+	Nod* next;
+};
+
+void afisareAngajat(Angajat angajat) {
+	printf("\n Angajatul %s are %d ani.", angajat.nume, angajat.varsta);
+}
+
+Angajat initializareAngajat(const char* nume, int varsta) {
+	Angajat angajat;
+	angajat.varsta = varsta;
+	angajat.nume = (char*)malloc(sizeof(char) * (strlen(nume) + 1));
+	strcpy(angajat.nume, nume);
+
+	return angajat;
+}
+
+void parcurgereLista(Nod* cap) {
+	while(cap) {
+		afisareAngajat(cap->info);
+		cap = cap->next;
+	}
+}
+
+void inserareInceput(Nod** cap, Angajat info) {
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->info = info;
+	nou->next = *cap;
+	*cap = nou;
+}
+
+void inserareLaFinal(Nod** cap, Angajat info) {
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->info = info;
+	nou->next = NULL;
+
+	if ((*cap) == NULL) {
+		*cap = nou;
+	}
+	else
+	{
+		Nod* temp = *cap;
+		while (temp->next)
+		{
+			temp = temp->next;
+		}
+		temp->next = nou;
+	}
+}
+
+void dezalocare(Nod** cap) {
+	while ((*cap)!=NULL)
+	{
+		Nod* temp = *cap;
+		*cap = (*cap)->next;
+		free(temp->info.nume);
+		free(temp);
+	}
+}
+// Funcția pentru calculul numărului de apariții a unui nume în listă
+
+int suma(Nod* cap, const char* nume) {
+	int suma = 0;
+
+	while (cap)
+	{
+		if (strcmp(cap->info.nume, nume) == 0)
+			suma++;
+		cap = cap->next;
+	}
+	return suma;
+}
+
+float calculVarstaMedie(Nod* cap) {
+	int contor = 0;
+	float medie = 0.0;
+	while (cap)
+	{
+		medie += cap->info.varsta;
+		contor++;
+		cap = cap->next;
+	}
+	if (contor == 0) {
+		return 0;
+
+	}
+	return medie / contor;
+}
+
+// Funcția pentru citirea angajaților dintr-un fișier text
+void citireFisierAngajati(const char* numeFisier, Nod** cap) {
+	FILE* fisier = fopen(numeFisier, "r");
+	if (fisier == NULL) {
+		printf("Eroare la deschiderea fisierului!!");
+		exit(1);
+	}
+	char nume[20];//buffer pt citirea numelui
+	int  varsta; //variabila pt citirea varstei
+
+	while (fscanf(fisier, "%s %d", nume, &varsta) != EOF)
+	{
+		inserareLaFinal(cap, initializareAngajat(nume, varsta));
+	}
+	fclose(fisier);
+}
+
+// Functie pentru gasirea angajatului cu varsta cea mai mare
+Angajat angajatCuVarstaMaxima(Nod* cap) {
+	Angajat angajatMaxim = cap->info;
+	while (cap)
+	{
+		if (cap->info.varsta > angajatMaxim.varsta) {
+			angajatMaxim = cap->info;
+		}
+		cap = cap->next;
+	}
+	return angajatMaxim;
+}
+// Functie pentru gasirea angajatului cu varsta cea mai mica
+Angajat angajatCuVarstaMinima(Nod* cap) {
+	Angajat angajatMinim = cap->info;
+	while (cap)
+	{
+		if (cap->info.varsta < angajatMinim.varsta) {
+			angajatMinim = cap->info;
+		}
+		cap = cap->next;
+	}
+	return angajatMinim;
+}
+// Functie pentru stergerea unui element de pe o pozitie specificata
+void stergereElementPozitie(Nod** cap, int pozitie) {
+	if (*cap == NULL) {
+		return;
+	}
+
+	Nod* temp = *cap;
+	if (pozitie == 0) {
+		*cap = temp->next;
+		free(temp->info.nume);
+		free(temp);
+		return;
+	}
+
+	for (int i = 0; i != NULL && i < pozitie - 1; i++) {
+		temp = temp->next;
+	}
+
+	if (temp == NULL || temp->next == NULL) {
+		return;
+	}
+
+	Nod* urmator = temp->next->next;
+	free(temp->next->info.nume);
+	free(temp->next);
+	temp->next = urmator;
+}
+
+// Functie pentru inserarea unui element la o anumita pozitie
+void inserarePePozitie(Nod** cap, Angajat info, int pozitie) {
+	if (pozitie < 0) {
+		printf("Pozitie invalida!");
+			return;
+	}
+
+	if (pozitie == 0) {
+		inserareInceput(cap, info);
+		return;
+	}
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	if (nou == NULL) {
+		printf("Eroare la alocarea memoriei!");
+		exit(1);
+	}
+	nou->info = info;
+
+	Nod* temp = *cap;
+	for (int i = 0; i < pozitie - 1 && temp != NULL; i++) {
+		temp = temp->next;
+	}
+
+	if (temp == NULL) {
+		printf("Pozitie depasita!");
+		return;
+	}
+	nou->next = temp->next;
+	temp->next = nou;
+}
+
+// Functie pentru conversia listei la vector
+Angajat* conversieListaLaVector(Nod* cap, int* dimensiune) {
+	int lungime = 0;
+	Nod* temp = cap;
+	while (temp!=NULL)
+	{
+		lungime++;
+		temp = temp->next;
+	}
+	*dimensiune = lungime;
+	Angajat* vector = (Angajat*)malloc(lungime * sizeof(Angajat));
+
+	int index = 0;
+	while (cap!= NULL)
+	{
+		vector[index++] = cap->info;
+		cap = cap->next;
+	}
+	return vector;
+}
+void main() {
+	Nod* cap = NULL;
+
+	citireFisierAngajati("angajati.txt", &cap);
+
+	printf("------------CIRIRE DE LA TASTATURA-------------");
+
+	inserareInceput(&cap, initializareAngajat("Mihai", 30));
+	inserareInceput(&cap, initializareAngajat("Andrei", 28));
+	inserareInceput(&cap, initializareAngajat("Ana", 29));
+	inserareInceput(&cap, initializareAngajat("Ana", 29));
+	inserareInceput(&cap, initializareAngajat("Rebeca", 29));
+
+	inserareLaFinal(&cap, initializareAngajat("Crina", 25));
+
+	parcurgereLista(cap);
+
+	printf("\n\n Numele \"Ana\" apare de %d ori.\n",suma( cap, "Ana"));
+
+	printf("\n\nMedia varstei angajatilor este %f", calculVarstaMedie(cap));
+
+	printf("\n\n");
+	Angajat angajatMax = angajatCuVarstaMaxima(cap);
+	printf("\n\n Angajatul cu varsta cea mai mare este : %s cu varsta %d ani.", angajatMax.nume, angajatMax.varsta);
+
+	stergereElementPozitie(&cap, 2);
+	printf("\n\n Lista dupa stergere: ");
+	parcurgereLista(cap);
+
+	Angajat angajatMin=	angajatCuVarstaMinima(cap);
+	printf("\n\n Angajatul cu varsta minima este : %s cu varsta %d ani.", angajatMin.nume, angajatMin.varsta);
+
+	inserarePePozitie(&cap, initializareAngajat("Nou", 22), 3);
+	printf("\n\nLista dupa inserare ");
+	parcurgereLista(cap);
+
+	int dimensiuneVector;
+	Angajat* vectorAngajat = conversieListaLaVector(cap, &dimensiuneVector);
+	printf("\n\n Vectorul de angajati: ");
+	for (int i = 0; i < dimensiuneVector; i++) {
+		printf("\n%s %d", vectorAngajat[i].nume, vectorAngajat[i].varsta);
+	}
+	dezalocare(&cap);
+	free(vectorAngajat);
+}
